74de95276d3a6c0a5325d9804a18859880c7685a
G484562024 <2484562024@st.takushoku-u.ac.jp>
2025-11-19 14:43:00 +0900

    コースL4

--- ap08L4.js
+++ ap08L4.js
@@ -17,8 +17,15 @@ let renderer;
 let camera;
 let course;
 export const origin = new THREE.Vector3();
+//コース調整部分
 export const controlPoints = [
     [-50, 20],
+    [-5,0],
+    [20,20],
+    [-4,20],
+    [37,-7],
+    [8,10],
+    [19,-19],
     [ 25,-40]
 ]
 export function init(scene, size, id, offset, texture) {
@@ -48,7 +55,34 @@ export function init(scene, size, id, offset, texture) {
     // ビル
 
     // コース(描画)
+    //制御補給による曲線
+    course=new THREE.CatmullRomCurve3(
+        controlPoints.map((p)=>{
+            return(new THREE.Vector3()).set(
+                offset.x+p[0],
+                0,
+                offset.z+p[1]
+            );
+        }),false
+    )
 
+    //曲線から100箇所を取り出して円を作る
+    const poonts = course.getPoints(100);
+    poonts.forEach((point)=>{
+        const road = new THREE.Mesh(
+            new THREE.CircleGeometry(5,16),
+            new THREE.MeshLambertMaterial({
+                color:"gray",
+            })
+        )
+        road.rotateX(-Math.PI/2);
+        road.position.set(
+            point.x,
+            0,
+            point.z
+        );
+        scene.add(road);
+    })
 }
 
 // コース(自動運転用)
@@ -62,6 +96,10 @@ export function getCamera() {
 
 // 車の設定
 export function setCar(scene, car) {
+    const SCALE = 0.01;
+    car.position.copy(origin);
+    car.scale.set(SCALE,SCALE,SCALE);
+    scene.add(car);
 }
 
 // Windowサイズの変更処理
