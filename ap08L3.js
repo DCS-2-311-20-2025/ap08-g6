6f586a7689602450ebf2befeae97a4fb5229f472
G484542024 <248454@st.takushoku-u.ac.jp>
2025-11-19 15:24:30 +0900

    コミット

--- ap08L3.js
+++ ap08L3.js
@@ -48,6 +48,35 @@ export function init(scene, size, id, offset, texture) {
     scene.add(plane);
 
     // ビル
+    function makeBuilding(x,z,type){
+        const height=[2,2,7,4,5];
+        const bldgH=height[type]*5;
+        const geometry=new THREE.BoxGeometry(8,bldgH,8);
+        const material=new THREE.MeshLambertMaterial({map:texture});
+        const sideUvS=(type*2+1)/11;
+        const sideUvE=(type*2+2)/11;
+        const topUvS=(type*2+3)/11;
+        const topUvE=(type*2+4)/11;
+        const uvs=geometry.getAttribute("uv");
+        for (let i=0;i<48;i+=4){
+            if(i<16||i>26){
+                uvs.array[i]=sideUvS;
+                uvs.array[i+2]=sideUvE;
+            }
+            else{
+                uvs.array[i]=topUvS;
+                uvs.array[i+2]=topUvE;
+            }
+        }
+        const bldg=new THREE.Mesh(
+            geometry,
+            material
+        )
+        bldg.position.set(x,0,z);
+        scene.add(bldg);
+    }
+    makeBuilding(-30,25,2);
+    makeBuilding(-70,25,3);
 
     // コース(描画)
     //制御点を補完して曲線を作る
