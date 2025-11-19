47da00b28dc00757c6239e2efb257b2d4ded7ecf
G484542024 <248454@st.takushoku-u.ac.jp>
2025-11-19 14:56:36 +0900

    コミット

--- ap08L3.js
+++ ap08L3.js
@@ -83,7 +83,7 @@ export function init(scene, size, id, offset, texture) {
 // コース(自動運転用)
 export function makeCourse(scene) {
     const courseVectors=[];
-    const parts=[L3,L4,L1.L2];
+    const parts=[L3,L4,L1,L2];
     parts.forEach((part)=>{
         part.controlPoints.forEach((p)=>{
             courseVectors.push(
